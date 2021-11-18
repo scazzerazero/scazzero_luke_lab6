bcef58834d1e032c53695008d303abbef2c6971c
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-18 22:19:55 +0000

    add firefly method and multiprocessing

--- displayClass.py
+++ displayClass.py
@@ -1,6 +1,6 @@
-import time
+import multiprocessing, time
 from shifterClass import Shifter #from shifter.py import Shifter class. Extended by compositon
-
+import random
 
 
 
@@ -17,21 +17,41 @@ class LED8x8():
     0b00111100 ]
 
   #pattern=(~pattern & 0b11111111)
-
-
-
+  row = [
+    0b10000000,
+    0b01000000,
+    0b00100000,
+    0b00010000,
+    0b00001000,
+    0b00000100,
+    0b00000010,
+    0b00000001
+  ]
+ 
 
 
   #'sequentially sends 8 pairs of bytes to a Shifter object'
 
   def __init__(self,data,latch,clock):  
     self.shifter=Shifter(data,latch,clock)
+    #in order to get a separate process to launch as soon as LED8x8 class is instantiated
+
+
+  def firefly(self):
+    # change by adding random number between -1 and 1. IF statement to restrict to boundaries to 8.
+    
+    numRow=3
+    numCol=3
+    
+    self.shifter.shiftByte(~LED8x8.row[numRow-1]) #load col values
+    self.shufter.shiftBytle(LED8x8.row[numCol-1]) #load row values
+    self.shifter.latch() #send to output
+    
     
   def setPattern(self, num):
     #self.shifter.shiftByte(~LED8x8.pattern[num] & 0b11111111)  #load the col values
     self.shifter.shiftByte(~LED8x8.pattern[num] & 0b11111111)  #load the col values
     self.shifter.shiftByte( 1 << num )    #load the row values 0b00000001, then 0b00000010, etc...
-
     self.shifter.latch()
 
 #to run this LED8x8(dataPin,latchPin,clockPin)
--- runme.py
+++ runme.py
@@ -1,11 +1,12 @@
 
-import time
+import  multiprocessing, time
+
 from displayClass import LED8x8
 import RPi.GPIO as GPIO #for cleanup()
 
 dataPin, latchPin, clockPin = 23, 24, 25 #input data, send data, load data
 
-theLED8x8= LED8x8(dataPin, latchPin, clockPin)
+theLED8x8= LED8x8(dataPin, latchPin, clockPin) #create object from led8x8 class
 
 # Simple demonstration of the LED8x8 class.
 # to define the GPIO pins, since LED8x8 is
@@ -13,9 +14,19 @@ theLED8x8= LED8x8(dataPin, latchPin, clockPin)
 
 while True:
   try:
-    for n in range(8):
-      theLED8x8.setPattern(n)
-      time.sleep(0.001)
+    x = [8,7,6,5,4,3,2,1]
+    p = multiprocessing.Process(target=theLED8x8.firefly, args=(x,))
+    p.daemon = True # Force process termination when main code ends
+    p.start()        # Start the process (only once!)
+    p.terminate()    # Terminate the process (no equivalent for threads)
+    #   (always 'join' after termination)
+    p.join()         # Force the calling process to wait for the new
+    #   process to end before continuing (or terminating)
+    p.join(2)        # Pause the calling process for up to n seconds to let
+    #   the process to end, then join even if not ended
+
+
+
   except KeyboardInterrupt:
     print("\nExiting!")
     GPIO.cleanup()
