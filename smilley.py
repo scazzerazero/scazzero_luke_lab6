e336a6153030736adeb187f1e0884099e948ee4e
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-18 19:50:05 +0000

    not reversing the patterN?

--- smilley.py
+++ smilley.py
@@ -20,54 +20,6 @@ class LED8x8():
 
 
 
-  '''col = [
-    0b01111111,
-    0b10111111,
-    0b11011111,
-    0b11101111,
-    0b11110111,
-    0b11111011,
-    0b11111101,
-    0b11111110]'''
-
-  '''col =[
-    0b11111110,
-    0b11111101,
-    0b11111011,
-    0b11110111,
-    0b11101111,
-    0b11011111,
-    0b10111111,
-    0b01111111]'''
-  col =[
-    0b10000000,
-    0b01000000,
-    0b00100000,
-    0b00010000,
-    0b00001000,
-    0b00000100,
-    0b00000010,
-    0b00000001]
-    
-  '''col = [
-  0b00000000,
-  0b00000000,
-  0b00000000,
-  0b00000000,
-  0b00000000,
-  0b00000000,
-  0b00000000,
-  0b00000000]'''
-  '''col = [
-    0b11111111,
-    0b11111111,
-    0b11111111,
-    0b11111111,
-    0b11111111,
-    0b11111111,
-    0b11111111,
-    0b11111111]'''
-
 
 
   #'sequentially sends 8 pairs of bytes to a Shifter object'
@@ -76,7 +28,9 @@ class LED8x8():
     self.shifter=Shifter(data,latch,clock)
     
   def setPattern(self, num):
-    self.shifter.shiftByte(~LED8x8.pattern[num] & 0b11111111)  #load the col values
+    #self.shifter.shiftByte(~LED8x8.pattern[num] & 0b11111111)  #load the col values
+    self.shifter.shiftByte(LED8x8.pattern[num])  #load the col values
+
     self.shifter.shiftByte( 1 << num )    #load the row values
 
     self.shifter.latch()
