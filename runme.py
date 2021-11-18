1641944df80b7b6b29580c97ac4ee0b3e4755647
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-18 22:22:27 +0000

    remove args from multiprocessing object

--- runme.py
+++ runme.py
@@ -14,21 +14,18 @@ theLED8x8= LED8x8(dataPin, latchPin, clockPin) #create object from led8x8 class
 
 while True:
   try:
-    x = [8,7,6,5,4,3,2,1]
-    p = multiprocessing.Process(target=theLED8x8.firefly, args=(x,))
+    p = multiprocessing.Process(target=theLED8x8.firefly)
     p.daemon = True # Force process termination when main code ends
     p.start()        # Start the process (only once!)
-    p.terminate()    # Terminate the process (no equivalent for threads)
-    #   (always 'join' after termination)
-    p.join()         # Force the calling process to wait for the new
-    #   process to end before continuing (or terminating)
-    p.join(2)        # Pause the calling process for up to n seconds to let
-    #   the process to end, then join even if not ended
 
 
 
   except KeyboardInterrupt:
     print("\nExiting!")
+    p.terminate()    # Terminate the process (no equivalent for threads)
+    #   (always 'join' after termination)
+    p.join(2)        # Pause the calling process for up to n seconds to let
+    #   the process to end, then join even if not ended
     GPIO.cleanup()
     break
 
