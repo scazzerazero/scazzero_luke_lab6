8e0af4acea7d69f3941ab1f6cc09ffdead97c400
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-11-19 16:28:17 +0000

    num

--- displayClass.py
+++ displayClass.py
@@ -56,9 +56,9 @@ class LED8x8(multiprocessing.Process):
       self.shifter.latch() #send to output
       time.sleep(0.10)''' #from V1
   def firefly(self, pattern):
-    for i in range(8):
-      self.shifter.shiftByte(~LED8x8.pattern[i]) #load col values
-      self.shifter.shiftByte(LED8x8.pattern[i]) #load row values
+    for num in range(8):
+      self.shifter.shiftByte(~LED8x8.pattern[num]) #load col values
+      self.shifter.shiftByte(1<<num) #load row values
       self.shifter.latch() #send to output
       time.sleep(0.010)
 
